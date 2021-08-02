e415dc1958b821c3cd89f11abc1cbcfa6538b60f
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-08-02 07:54:50 -0300

    importar Contatos

--- Contatos/app/src/main/java/com/example/contatos/ContactsAdapter.kt
+++ Contatos/app/src/main/java/com/example/contatos/ContactsAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.contatos
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactsAdapter (val contactsList: ArrayList<Contact>):
     RecyclerView.Adapter<ContactsAdapter.ViewHolder>(){
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactsAdapter.ViewHolder, position: Int) {
@@ -17,5 +21,16 @@ class ContactsAdapter (val contactsList: ArrayList<Contact>):
 
     override fun getItemCount(): Int {
         return contactsList.size
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bindItem(contact: Contact){
+            val textName = itemView.findViewById<TextView>(R.id.tv_contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.tv_contact_phone_number)
+
+            textName.text = contact.name
+            textPhone.text = contact.phoneNumber
+        }
     }
 }
